6a4ee83fd906465c3a3991913bc5261d6543b835
kitswas <90329875+kitswas@users.noreply.github.com>
2022-12-24 23:56:31 +0530

    Red Black Tree skeleton

---
+++ src/rbtree.cpp
@@ -0,0 +1,95 @@
+/**
+ * Reference: https://www.programiz.com/dsa/red-black-tree
+ */
+
+#include "rbtree.hpp"
+
+template <typename T>
+RBTree<T>::RBTree(int (*comparator)(const T &a, const T &b))
+{
+	this->compare = comparator;
+	this->elements = 0;
+}
+
+template <typename T>
+RBTree<T>::~RBTree()
+{
+}
+
+/**
+ * @brief Left rotate the tree at the given node
+ *
+ * @details
+ *
+ *    x             y
+ *   / \           / \
+ *  a   y   ->    x   c
+ *     / \       / \
+ *    b   c     a   b
+ * @tparam T
+ * @param node
+ */
+template <typename T>
+void RBTree<T>::leftRotate(std::shared_ptr<Node> node)
+{
+	std::shared_ptr<Node> right = node->right;
+	if (node == nullptr || right == nullptr)
+		return;
+	node->right = right->left;
+	if (right->left != nullptr)
+		right->left->parent = node;
+	right->left = node;
+	right->parent = node->parent;
+	if (node->parent == nullptr)
+	{
+		this->root = right;
+	}
+	else if (node == node->parent->left)
+	{
+		node->parent->left = right;
+	}
+	else
+	{
+		node->parent->right = right;
+	}
+	node->parent = right;
+}
+
+/**
+ * @brief Right rotate the tree at the given node
+ *
+ * @details
+ *
+ *     x             y
+ *    / \           / \
+ *   y   c   ->    a   x
+ *  / \               / \
+ * a   b             b   c
+ * @tparam T
+ * @param node
+ */
+template <typename T>
+void RBTree<T>::rightRotate(std::shared_ptr<Node> node)
+{
+	std::shared_ptr<Node> left = node->left;
+	if (node == nullptr || left == nullptr)
+		return;
+	node->left = left->right;
+	if (left->right != nullptr)
+		left->right->parent = node;
+	left->right = node;
+	left->parent = node->parent;
+	if (node->parent == nullptr)
+	{
+		this->root = left;
+	}
+	else if (node == node->parent->left)
+	{
+		node->parent->left = left;
+	}
+	else
+	{
+		node->parent->right = left;
+	}
+	node->parent = left;
+}
---
+++ src/rbtree.hpp
@@ -0,0 +1,110 @@
+#ifndef RBTREE_H
+#define RBTREE_H
+
+#include <iostream>
+#include <memory>
+
+/**
+ * @brief A red-black tree implementation
+ *
+ * @tparam T The type of the elements in the tree
+ */
+template <typename T>
+class RBTree
+{
+private:
+	struct Node
+	{
+		T data;
+		std::shared_ptr<Node> left;
+		std::shared_ptr<Node> right;
+		std::shared_ptr<Node> parent;
+		bool isRed;
+		Node(const T &data, std::shared_ptr<Node> parent) : data(data), left(nullptr), right(nullptr), parent(parent), isRed(true) {}
+	};
+
+	std::shared_ptr<Node> root;
+	int (*compare)(const T &a, const T &b);
+	size_t elements;
+
+	void leftRotate(std::shared_ptr<Node> node);
+	void rightRotate(std::shared_ptr<Node> node);
+
+public:
+	/**
+	 * @brief Construct a new RBTree object
+	 *
+	 * @param comparator A function pointer to a function that compares two elements
+	 */
+	RBTree(int (*comparator)(const T &a, const T &b));
+
+	/**
+	 * @brief Destroy the RBTree object
+	 *
+	 */
+	~RBTree();
+
+	/**
+	 * @brief Insert an element into the tree
+	 *
+	 * @param data The element to insert
+	 */
+	void insert(const T &data);
+
+	/**
+	 * @brief Remove an element from the tree
+	 *
+	 * @param data The element to remove
+	 */
+	void remove(const T &data);
+
+	/**
+	 * @brief Check if the tree contains an element
+	 *
+	 * @param data The element to check for
+	 * @return true If the tree contains the element
+	 * @return false If the tree does not contain the element
+	 */
+	bool contains(const T &data);
+
+	/**
+	 * @brief Get the number of elements in the tree
+	 *
+	 * @return size_t The number of elements in the tree
+	 */
+	size_t size();
+
+	/**
+	 * @brief Get the height of the tree
+	 *
+	 * @return size_t The height of the tree
+	 */
+	size_t height();
+
+	/**
+	 * @brief Extract the minimum element from the tree
+	 *
+	 * @return T The minimum element
+	 */
+	T extractMin();
+
+	/**
+	 * @brief Print preorder traversal of the tree
+	 *
+	 */
+	void printPreorder();
+
+	/**
+	 * @brief Print inorder traversal of the tree
+	 *
+	 */
+	void printInorder();
+
+	/**
+	 * @brief Print postorder traversal of the tree
+	 *
+	 */
+	void printPostorder();
+};
+
+#endif // RBTREE_H
